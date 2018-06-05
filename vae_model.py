8b654a9f708f492f08ed807b842ff18e2522bff2
augustdemi <augustdemi@gmail.com>
2018-06-05 19:01:54 -0400

    delete some prints

--- vae_model.py
+++ vae_model.py
@@ -17,13 +17,10 @@ class VAE:
         self.img_shape = img_shape,
         self.label_shape =label_shape,
 
-        w_1 = 1 / 50
         batch_size=2
         latent_dim = 2000
         target_std_vec = np.ones(latent_dim)
         target_mean_vec = np.zeros(latent_dim)
-
-
 
 
 
@@ -42,7 +39,6 @@ class VAE:
 
 
         emb = Dropout(0.5)(emb)
-        print("emb: ", emb)
         z_mean      = Dense(latent_dim)(emb) # latent_dim는 output space의 dim이 될것. activation함수는 none임 out_1(라벨값 y)을 위한 layer쌓는중  classifier?????????????
         z_log_sigma = Dense(latent_dim)(emb) #
 
@@ -56,8 +52,6 @@ class VAE:
 
         z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_sigma]) # 발굴한 feature space에다 노이즈까지 섞어서 샘플링한 z
 
-        print("Z: ", z)
-        print(z.get_shape())
         resized_z = Reshape((2000, 1))(z_mean)
         out_1 = EE.layers.softmaxPDF(label_shape[0], label_shape[1])(resized_z)
 
@@ -69,29 +63,6 @@ class VAE:
 
         out_0 = EE.networks.decoder(x_decoded_mean, shape, norm=1) # 위에서만든 layer로 디코더 실행. 근데 사실상 이 디코더에 오기까지 오리지날 트레인 x를 인코드하는거부터 시작됨. vae.
 
-
-        def vae_loss(img, rec):
-            print("img", img)
-            print("rec", rec)
-            print(">>>>>>>>>> vae loss")
-            kl_loss = - 0.5 * KB.mean(1 + z_log_sigma - KB.square(z_mean) - KB.exp(z_log_sigma), axis=-1)
-            return w_1*kl_loss
-
-        def rec_loss(img, rec):
-            print("img", img)
-            print("rec", rec)
-            print(">>>>>>>>>> rec loss")
-            mse = EE.losses.mse(img, rec)
-            return mse
-
-        def pred_loss(y_true, y_pred):
-            print("y_true", y_true)
-            print("y_pred", y_pred)
-            print(">>>>>>>>> pred loss")
-            ce = EE.losses.categorical_crossentropy(y_true, y_pred)
-            return (1 - w_1) * ce
-
-        loss = [rec_loss, pred_loss, vae_loss]
 
         model_train = K.models.Model([inp_0], [out_0, out_1, out_0]) #inp_0: train data, out_0 : reconstruted img, out_1: predicted label. (vae)에서 쌓은 레이어로 모델만듦
         model_z_int = K.models.Model([inp_0], [z_mean, out_1])
