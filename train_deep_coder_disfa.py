c4f61a02aa1f856149707fa946296ec9d9eb8f6d
augustdemi <augustdemi@gmail.com>
2018-06-19 18:15:52 -0400

    minor change

--- train_deep_coder_disfa.py
+++ train_deep_coder_disfa.py
@@ -94,6 +94,7 @@ z_log_sigma = Dense(latent_dim)(emb) #
 
 def sampling(args): ########### input param의 평균과 분산에 noise(target_mean, sd 기준)가 섞인 샘플링 값을줌
     import keras.backend as KB
+    batch_size = 10
     z_mean, z_log_sigma = args
     epsilon = []
     for m, s in zip(np.ones(2000), np.zeros(2000)):
