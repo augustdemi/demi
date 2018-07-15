cc5403643e98f6427c11907b0197bd2e5144e977
augustdemi <augustdemi@gmail.com>
2018-07-15 18:26:50 -0400

    change batch size

--- data_generator.py
+++ data_generator.py
@@ -85,9 +85,9 @@ class DataGenerator(object):
         # inputa_files has (n*k * num_of_task) files.
         # make it to batch of which size is (n*k) : thus, the total number of batch = num_of_task
         # batch_size = int(self.num_classes * FLAGS.update_batch_size)
-        batch_size = 5
+        batch_size = 10
         N_batch = int(len(inputa_files) / batch_size)  # len(inputa_files)/nk = num of task
-        num_of_task = N_batch / int(self.num_classes * FLAGS.update_batch_size)
+        num_of_task = int(N_batch / int(self.num_classes * FLAGS.update_batch_size))
         vae_model = VAE((self.img_size[0], self.img_size[1], 1), batch_size)
 
         def latent_feature(file_names):
