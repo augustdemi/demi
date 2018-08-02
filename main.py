7a8e60492d12601b0eb351563fe1346ee71c1d16
augustdemi <augustdemi@gmail.com>
2018-08-02 15:10:12 -0400

    gpu

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ from maml import MAML
 from tensorflow.python.platform import flags
 from datetime import datetime
 import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import pickle
 
 start_time = datetime.now()
