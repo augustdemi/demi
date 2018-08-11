35ddc9476c42773707ec0a6c7601181cfce50bd9
augustdemi <augustdemi@gmail.com>
2018-08-11 03:52:40 -0400

    test_a.b가 픽스 되도록 수정

--- makeData/maml/split_test_task.py
+++ makeData/maml/split_test_task.py
@@ -11,19 +11,26 @@ original_frame_path = "/home/ml1323/project/robert_data/DISFA/detected_disfa/"
 all_au = ['au1', 'au2', 'au4', 'au5', 'au6', 'au9', 'au12', 'au15', 'au17', 'au20', 'au25', 'au26']
 test_subjects = ['SN017', 'SN018', 'SN021', 'SN023', 'SN024', 'SN025', 'SN026', 'SN027', 'SN028', 'SN029', 'SN030',
                  'SN031', 'SN032']
-for au in all_au:
-    for subject in test_subjects:
-        detected_img_files = os.listdir(original_frame_path + subject)
-        detected_frame_idx = [int(elt.split('frame')[1].split('_')[0]) for elt in detected_img_files]
-        detected_frame_idx = list(set(detected_frame_idx))
-        if type(len(detected_frame_idx) / 2) is not int: detected_frame_idx = detected_frame_idx[:-1]
-        min_len = len(detected_frame_idx)
-        print('min_len: ', min_len)
-        test_a_idx = random.sample(detected_frame_idx, int(min_len / 2))
-        test_b_idx = [i for i in detected_frame_idx if i not in test_a_idx]
-        print('test_a len: ', len(test_a_idx))
-        print('test_b len: ', len(test_b_idx))
 
+for subject in test_subjects:
+    detected_img_files = os.listdir(original_frame_path + subject)
+    detected_frame_idx = [int(elt.split('frame')[1].split('_')[0]) for elt in detected_img_files]
+    detected_frame_idx = list(set(detected_frame_idx))
+    if type(len(detected_frame_idx) / 2) is not int: detected_frame_idx = detected_frame_idx[:-1]
+    min_len = len(detected_frame_idx)
+    print('min_len: ', min_len)
+    test_a_idx = random.sample(detected_frame_idx, int(min_len / 2))
+    test_b_idx = [i for i in detected_frame_idx if i not in test_a_idx]
+    print('test_a len: ', len(test_a_idx))
+    print('test_b len: ', len(test_b_idx))
+    print('-------------------------------------------------')
+    print(test_a_idx)
+    print('-------------------------------------------------')
+    print(test_b_idx)
+    ########### 위의 각 subject 별 fixed test_a, test_b에 대해서, 각 au별로 on/off를 구해 분리해둠.
+    ########### 그리곤 test_b셋에대해서 testset pickle값을 만들어 둬서 이 전체 데이터 셋에 대해서 evaluation할 것임.
+
+    for au in all_au:
         label_path = "/home/ml1323/project/robert_data/DISFA/label/" + subject + "/" + subject + "_" + au + ".txt"
         intensities_for_one_au = []
         f = open(label_path, 'r')
