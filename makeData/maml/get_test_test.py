8d81f848061d72705028ba85eb93e131f85d2500
augustdemi <augustdemi@gmail.com>
2018-08-14 12:21:08 -0400

    make it for test_b in new_dataset

--- makeData/maml/get_test_test.py
+++ makeData/maml/get_test_test.py
@@ -2,37 +2,53 @@ import pickle
 import os
 import numpy as np
 
-au = "au25"
-path = "/home/ml1323/project/robert_data/DISFA/kshot_rest/" + au
+path = "/home/ml1323/project/robert_data/DISFA/new_dataset/test_b/"
 save_path = path + "/testset/"
 if not os.path.exists(save_path): os.makedirs(save_path)
 
-path += "/rest"
 subjects = os.listdir(path)
 subjects.sort()
+all_au = ['au1', 'au2', 'au4', 'au5', 'au6', 'au9', 'au12', 'au15', 'au17', 'au20', 'au25', 'au26']
 
-for subject in subjects[14:27]:
+for subject in subjects:
     subject_folder = os.path.join(path, subject)
-    test_file_names = []
-    y_lab = []
-    off_files = os.listdir(subject_folder + "/off")
-    test_file_names.extend([os.path.join(subject_folder + "/off", off_file) for off_file in off_files])
-    on_files = os.listdir(subject_folder + "/on")
-    test_file_names.extend([os.path.join(subject_folder + "/on", on_file) for on_file in on_files])
+    files = os.listdir(subject_folder)
+    test_file_names = [os.path.join(subject_folder, file) for file in files]
+    print('files in ', subject, ' : ', len(test_file_names))
+    frame_idx = [int(file.split('frame')[1].split('.')[0]) for file in files]
 
-    lab = np.zeros((len(off_files), 2))
-    lab[:, 0] = 1
-    y_lab.extend(lab)
-    lab = np.zeros((len(on_files), 2))
-    lab[:, 1] = 1
-    y_lab.extend(lab)
+    label_per_subject = []
+    for au in all_au:
+        label_path = "/home/ml1323/project/robert_data/DISFA/label/" + subject + "/" + subject + "_" + au + ".txt"
+        intensities_for_one_au = []
+        f = open(label_path, 'r')
+        all_labels = f.readlines()
+
+        label_per_au = []
+        for idx in frame_idx:
+            try:
+                intensity = int(all_labels[idx].split(",")[1].split("\n")[0])
+            except:
+                print("on_idx - out of index: ", idx, " for au, subject: ", au, subject)
+                continue
+            if intensity > 0:
+                label_per_au.append(1)
+            else:
+                label_per_au.append(0)
+        # label_per_au = (4800,2)
+        label_per_subject.append(label_per_au)  # (12, 4800,2)
+    label_per_subject = np.array(label_per_subject)
+    print('label_per_subject shape: ', label_per_subject.shape)
+    print(label_per_subject[:3])
+    label_per_subject = np.transpose(label_per_subject, (1, 0, 2))  # (4800,12,2)
+    print('label_per_subject shape after : ', label_per_subject.shape)
+    print(label_per_subject[:3])
+    print('>>>>>>>>>>>>>>>>>>>>>>>> sub done:', subject)
+
 
     np.random.seed(3)
     np.random.shuffle(test_file_names)
-    y_lab = np.array(y_lab)
-    y_lab = y_lab.reshape(y_lab.shape[0], 1, 2)
     np.random.seed(3)
-    np.random.shuffle(y_lab)
+    np.random.shuffle(label_per_subject)
     out = open(save_path + subject + ".pkl", 'wb')
-    pickle.dump({'test_file_names': test_file_names, 'y_lab': y_lab}, out, protocol=2)
-
+    pickle.dump({'test_file_names': test_file_names, 'lab': label_per_subject}, out, protocol=2)
