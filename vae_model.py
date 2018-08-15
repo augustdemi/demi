d983b06f57c3e3d12c81c3c36fb8ade40a227ac3
augustdemi <augustdemi@gmail.com>
2018-08-15 10:56:15 -0400

    laodweight but수정

--- vae_model.py
+++ vae_model.py
@@ -69,10 +69,12 @@ class VAE:
     def loadWeight(self, vae_model, w=None, b=None):
         self.model_train.load_weights(vae_model)
         print("loaded weight from robert : ", self.model_train.get_weights()[58], self.model_train.get_weights()[59])
-        if (w.shape[1] == b.shape[0] == 12):
-            self.model_train.layers[-1].weights[0].load(w)
-            self.model_train.layers[-1].weights[1].load(b)
-            print("loaded weight from maml : ", self.model_train.get_weights()[58], self.model_train.get_weights()[59])
+        if w is not None and b is not None:
+            if w.shape[1] == b.shape[0] == 12:
+                self.model_train.layers[-1].weights[0].load(w)
+                self.model_train.layers[-1].weights[1].load(b)
+                print("loaded weight from maml : ", self.model_train.get_weights()[58],
+                      self.model_train.get_weights()[59])
 
     # only for test_test. 로드한 weight으로 pred값 도출. 배치로 한방에 predict하기 위해 로버트 모델을 쓴것.
     def testWithSavedModel(self, x):
