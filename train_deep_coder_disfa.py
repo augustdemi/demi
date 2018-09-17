aa2da733562cc35e020376376589c446e32353ce
augustdemi <augustdemi@gmail.com>
2018-09-17 11:15:03 -0400

    early stopping default

--- train_deep_coder_disfa.py
+++ train_deep_coder_disfa.py
@@ -204,7 +204,7 @@ model_train.compile(
 
 from keras.callbacks import EarlyStopping
 
-early_stopping = EarlyStopping(monitor='val_loss', patient=10, verbose=1)
+early_stopping = EarlyStopping()
 
 model_train.fit_generator(
         generator = GEN_TR,
