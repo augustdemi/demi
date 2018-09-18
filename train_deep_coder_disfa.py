7442f7ac46f7cddaec4b52b939c595f7fc4ea3a7
augustdemi <augustdemi@gmail.com>
2018-09-18 19:57:21 -0400

    sample per epoch

--- train_deep_coder_disfa.py
+++ train_deep_coder_disfa.py
@@ -219,7 +219,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
 model_train.fit_generator(
         generator = GEN_TR,
-    samples_per_epoch=3200,  # number of samples to process before going to the next epoch.
+    samples_per_epoch=960,  # number of samples to process before going to the next epoch.
         validation_data=GEN_TE, # integer, total number of iterations on the data.
         nb_val_samples = 5000, # number of samples to use from validation generator at the end of every epoch.
     initial_epoch=args.init_epoch,
