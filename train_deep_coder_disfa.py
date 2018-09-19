0d2f5a35fa633a388b753342a6b0ef7b8fb9c87a
augustdemi <augustdemi@gmail.com>
2018-09-19 11:10:31 -0400

    label revert

--- train_deep_coder_disfa.py
+++ train_deep_coder_disfa.py
@@ -83,12 +83,11 @@ def generator(dat_dict, aug, mod=0, s=False):
                 preprocessing=True,
                 augmentation=aug)
         # lab = lab.argmax(2)
-        lab = lab
-        # if au_index == 12:
-        #     lab = lab
-        # else:
-        #     lab = lab[:, au_index]
-        #     lab = np.reshape(lab, (lab.shape[0], 1, lab.shape[1]))
+        if au_index == 12:
+            lab = lab
+        else:
+            lab = lab[:, au_index]
+            lab = np.reshape(lab, (lab.shape[0], 1, lab.shape[1]))
         if mod==1:
             if(s): yield [img], [lab], [sub]
             else: yield [img], [lab]
