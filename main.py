8e0da3815c42a3b16c1311dcd64a216734e75437
augustdemi <augustdemi@gmail.com>
2018-10-07 01:45:58 -0400

    remove evaluation

--- main.py
+++ main.py
@@ -81,10 +81,7 @@ flags.DEFINE_bool('test_test', False, 'test the test set with test-model')
 flags.DEFINE_bool('test_train', False, 'test the test set with train-model')
 # for train, train_test
 flags.DEFINE_integer('sbjt_start_idx', 0, 'starting subject index')
-# for test_test, test_train
-flags.DEFINE_integer('num_test_tasks', 1, 'num of task for test')
-flags.DEFINE_string('testset_dir', './data/1/', 'directory for test set')
-flags.DEFINE_string('test_result_dir', 'robert', 'directory for test result log')
+
 # for train_test, test_test
 flags.DEFINE_string('keep_train_dir', None,
                     'directory to read already trained model when training the model again with test set')
@@ -96,9 +93,7 @@ flags.DEFINE_integer('au_idx', 12, 'au index to use in the given AE')
 flags.DEFINE_string('vae_model', './model_au_12.h5', 'vae model dir from robert code')
 flags.DEFINE_string('vae_model_for_test', './model_au_12.h5', 'vae model dir from robert code')
 flags.DEFINE_string('gpu', "0,1,2,3", 'vae model dir from robert code')
-flags.DEFINE_bool('global_test', False, 'get test evaluation throughout all test tasks')
-flags.DEFINE_bool('global_model', True, 'model is trained with all train/test tasks')
-flags.DEFINE_bool('robert', False, 'model is trained with all train/test tasks')
+
 
 def train(model, saver, sess, trained_model_dir, metatrain_input_tensors, metaval_input_tensors, resume_itr=0):
     print("===============> Final in weight: ", sess.run('model/w1:0').shape, sess.run('model/b1:0').shape)
@@ -247,182 +242,6 @@ def train(model, saver, sess, trained_model_dir, metatrain_input_tensors, metava
                 saver.save(sess, FLAGS.logdir + '/' + trained_model_dir + '/model' + str(itr))
 
 
-def test_each_subject(w, b, sbjt_start_idx):  # In case when test the model with the whole rest frames
-    from vae_model import VAE
-    import EmoData as ED
-    import cv2
-    import pickle
-    batch_size = 10
-    vae_model = VAE((160, 240, 1), batch_size, 1)
-    vae_model.loadWeight(FLAGS.vae_model_for_test, w, b)
-
-    pp = ED.image_pipeline.FACE_pipeline(
-        histogram_normalization=True,
-        grayscale=True,
-        resize=True,
-        rotation_range=3,
-        width_shift_range=0.03,
-        height_shift_range=0.03,
-        zoom_range=0.03,
-        random_flip=True,
-    )
-
-    def get_y_hat(file_names):
-        nb_samples = len(file_names)
-        t0, t1 = 0, batch_size
-        yhat = []
-        while True:
-            t1 = min(nb_samples, t1)
-            file_names_batch = file_names[t0:t1]
-            imgs = [cv2.imread(filename) for filename in file_names_batch]
-            img_arr, pts, pts_raw = pp.batch_transform(imgs, preprocessing=True, augmentation=False)
-            pred = vae_model.testWithSavedModel(img_arr)
-            yhat.extend(pred)
-            if t1 == nb_samples: break
-            t0 += batch_size  # 작업한 배치 사이즈만큼 t0와 t1늘림
-            t1 += batch_size
-        return np.array(yhat)
-
-    test_subjects = os.listdir(FLAGS.testset_dir)
-    test_subjects.sort()
-
-    test_subjects = test_subjects[sbjt_start_idx:sbjt_start_idx + 1]
-
-    print("test_subjects: ", test_subjects)
-
-    y_hat_all = []
-    y_lab_all = []
-    for test_subject in test_subjects:
-        print("============> subject: ", test_subject)
-        data = pickle.load(open(FLAGS.testset_dir + test_subject, "rb"), encoding='latin1')
-        test_file_names = data['test_file_names']
-        y_hat = get_y_hat(test_file_names)
-    lab = data['lab'][:, FLAGS.au_idx]
-    y_lab = np.reshape(lab, (lab.shape[0], 1, lab.shape[1]))
-    return y_hat, y_lab
-
-
-def test_robert():  # In case when test the model with the whole rest frames
-    from vae_model import VAE
-    import EmoData as ED
-    import cv2
-    import pickle
-    batch_size = 10
-    vae_model = VAE((160, 240, 1), batch_size, 1)
-    if 'tine' in FLAGS.vae_model:
-        vae_model.loadWeight(FLAGS.vae_model, None, None, 1)
-    else:
-        vae_model.loadWeight(FLAGS.vae_model, None, None)
-
-    pp = ED.image_pipeline.FACE_pipeline(
-        histogram_normalization=True,
-        grayscale=True,
-        resize=True,
-        rotation_range=3,
-        width_shift_range=0.03,
-        height_shift_range=0.03,
-        zoom_range=0.03,
-        random_flip=True,
-    )
-
-    def get_y_hat(file_names):
-        nb_samples = len(file_names)
-        t0, t1 = 0, batch_size
-        yhat = []
-        while True:
-            t1 = min(nb_samples, t1)
-            file_names_batch = file_names[t0:t1]
-            imgs = [cv2.imread(filename) for filename in file_names_batch]
-            img_arr, pts, pts_raw = pp.batch_transform(imgs, preprocessing=True, augmentation=False)
-            pred = vae_model.testWithSavedModel(img_arr)
-            yhat.extend(pred)
-            if t1 == nb_samples: break
-            t0 += batch_size  # 작업한 배치 사이즈만큼 t0와 t1늘림
-            t1 += batch_size
-        return np.array(yhat)
-
-    test_subjects = os.listdir(FLAGS.testset_dir)
-    test_subjects.sort()
-
-    test_subjects = test_subjects[0:FLAGS.num_test_tasks]
-
-    print("test_subjects: ", test_subjects)
-
-    y_hat_all = []
-    y_lab_all = []
-    for test_subject in test_subjects:
-        print("============> subject: ", test_subject)
-        data = pickle.load(open(FLAGS.testset_dir + test_subject, "rb"), encoding='latin1')
-        test_file_names = data['test_file_names']
-        y_hat = get_y_hat(test_file_names)
-        lab = data['lab'][:, FLAGS.au_idx]
-        y_lab = np.reshape(lab, (lab.shape[0], 1, lab.shape[1]))
-        y_hat_all.append(y_hat)
-        y_lab_all.append(y_lab)
-        print("y_hat shape:", y_hat.shape)
-        print("y_lab shape:", y_lab.shape)
-        print(">> y_hat_all shape:", np.vstack(y_hat_all).shape)
-        print(">> y_lab_all shape:", np.vstack(y_lab_all).shape)
-    print_summary(np.vstack(y_hat_all), np.vstack(y_lab_all), log_dir="./logs/result/test.txt")
-
-
-
-def test_all(w, b, trained_model_dir):  # In case when test the model with the whole rest frames
-    from vae_model import VAE
-    import EmoData as ED
-    import cv2
-    import pickle
-    batch_size = 10
-    vae_model = VAE((160, 240, 1), batch_size, 12)
-    vae_model.loadWeight(FLAGS.vae_model, w, b)
-
-    pp = ED.image_pipeline.FACE_pipeline(
-        histogram_normalization=True,
-        grayscale=True,
-        resize=True,
-        rotation_range=3,
-        width_shift_range=0.03,
-        height_shift_range=0.03,
-        zoom_range=0.03,
-        random_flip=True,
-    )
-
-    def get_y_hat(file_names):
-        nb_samples = len(file_names)
-        t0, t1 = 0, batch_size
-        yhat = []
-        while True:
-            t1 = min(nb_samples, t1)
-            file_names_batch = file_names[t0:t1]
-            imgs = [cv2.imread(filename) for filename in file_names_batch]
-            img_arr, pts, pts_raw = pp.batch_transform(imgs, preprocessing=True, augmentation=False)
-            pred = vae_model.testWithSavedModel(img_arr)
-            yhat.extend(pred)
-            if t1 == nb_samples: break
-            t0 += batch_size  # 작업한 배치 사이즈만큼 t0와 t1늘림
-            t1 += batch_size
-        return np.array(yhat)
-
-    test_subjects = os.listdir(FLAGS.testset_dir)
-    test_subjects.sort()
-    test_subjects = test_subjects[FLAGS.sbjt_start_idx:FLAGS.sbjt_start_idx + FLAGS.num_test_tasks]
-    print("test_subjects: ", test_subjects)
-
-    for test_subject in test_subjects:
-        print("============> subject: ", test_subject)
-        data = pickle.load(open(FLAGS.testset_dir + test_subject, "rb"), encoding='latin1')
-        test_file_names = data['test_file_names']
-        y_hat = get_y_hat(test_file_names)
-        save_path = "./logs/result/test_test/" + trained_model_dir
-        if FLAGS.test_train:
-            save_path = "./logs/result/test_train/" + trained_model_dir
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        print_summary(y_hat, data['lab'],
-                      log_dir=save_path + "/" + test_subject.split(".")[0] + ".txt")
-
-
-
 
 def main():
     os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.gpu
@@ -512,140 +331,6 @@ def main():
         print('updated weights from vae?: ', FLAGS.init_weight, sess.run(model.weights['w1']), sess.run('model/b1:0'))
     print("========================================================================================")
 
-    ################## Test ##################
-    if FLAGS.test_train or FLAGS.test_test:
-        def process(sbjt_start_idx):
-            if FLAGS.test_test:
-                if FLAGS.global_model:  # 모델이 모든 train or test tasks로 학습된거기때문에 항상 0부터 meta_batch_size까지 이용해서 구해진거가됨
-                    trained_model_dir = FLAGS.keep_train_dir + '/' + 'sbjt' + str(0) + ':' + str(
-                        FLAGS.meta_batch_size) + '.ubs_' + str(
-                        FLAGS.train_update_batch_size) + '.numstep' + str(FLAGS.num_updates) + '.updatelr' + str(
-                        FLAGS.train_update_lr) + '.metalr' + str(FLAGS.meta_lr)
-                else:
-                    trained_model_dir = FLAGS.keep_train_dir + '/' + 'sbjt' + str(sbjt_start_idx) + ':' + str(
-                        FLAGS.meta_batch_size) + '.ubs_' + str(
-                        FLAGS.train_update_batch_size) + '.numstep' + str(FLAGS.num_updates) + '.updatelr' + str(
-                        FLAGS.train_update_lr) + '.metalr' + str(FLAGS.meta_lr)
-            else:
-                trained_model_dir = 'cls_' + str(FLAGS.num_classes) + '.mbs_' + str(
-                    FLAGS.meta_batch_size) + '.ubs_' + str(
-                    FLAGS.train_update_batch_size) + '.numstep' + str(FLAGS.num_updates) + '.updatelr' + str(
-                    FLAGS.train_update_lr) + '.metalr' + str(FLAGS.meta_lr) + '.initweight' + str(FLAGS.init_weight)
-            #all_au = ['au1', 'au2', 'au4', 'au5', 'au6', 'au9', 'au12', 'au15', 'au17', 'au20', 'au25', 'au26']
-            all_au = ['au12']
-            w_arr = None
-            b_arr = None
-            for au in all_au:
-                model_file = None
-                print('--------- model file dir: ', FLAGS.logdir + '/' + au + '/' + trained_model_dir)
-                model_file = tf.train.latest_checkpoint(FLAGS.logdir + '/' + au + '/' + trained_model_dir)
-                print(">>>> model_file from ", au, ": ", model_file)
-                if (model_file == None):
-                    print(
-                        "############################################################################################")
-                    print("####################################################################### None for ", au)
-                    print(
-                        "############################################################################################")
-                else:
-                    if FLAGS.test_iter > 0:
-                        files = os.listdir(model_file[:model_file.index('model')])
-                        if 'model' + str(FLAGS.test_iter) + '.index' in files:
-                            model_file = model_file[:model_file.index('model')] + 'model' + str(FLAGS.test_iter)
-                            print(">>>> model_file2: ", model_file)
-                        else:
-                            print(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", files)
-                    print("Restoring model weights from " + model_file)
-                    print('---------------------------------------------')
-                    print(sess.run('model/w1:0'))
-                    print(sess.run('model/b1:0'))
-                    print(sess.run('model/w1:0').shape)
-                    print(sess.run('model/b1:0').shape)
-                    print('---------------------------------------------')
-
-                    saver.restore(sess, model_file)
-                    w = sess.run('model/w1:0')
-                    b = sess.run('model/b1:0')
-                    if w_arr is None:
-                        w_arr = w
-                        b_arr = b
-                    else:
-                        w_arr = np.hstack((w_arr, w))
-                        b_arr = np.vstack((b_arr, b))
-                    print("updated weights from ckpt: ", w, b)
-                    print('----------------------------------------------------------')
-            return test_each_subject(w_arr, b_arr, sbjt_start_idx)
-
-        if FLAGS.global_test:
-            if FLAGS.robert:
-                test_robert()
-            else:
-                print(
-                    "<<<<<<<<<<<< want to see the evaluation by concatenating all subject's predictions >>>>>>>>>>>>>>>>>")
-                save_path = "./logs/result/test_test/" + trained_model_dir
-                if FLAGS.test_train:
-                    save_path = "./logs/result/test_train/" + trained_model_dir
-                if not os.path.exists(save_path):
-                    os.makedirs(save_path)
-                y_hat = []
-                y_lab = []
-                for i in range(FLAGS.sbjt_start_idx, FLAGS.num_test_tasks):
-                    result = process(i)
-                    y_hat.append(result[0])
-                    y_lab.append(result[1])
-                    print("y_hat shape:", result[0].shape)
-                    print("y_lab shape:", result[1].shape)
-                    print(">> y_hat_all shape:", np.vstack(y_hat).shape)
-                    print(">> y_lab_all shape:", np.vstack(y_lab).shape)
-                print_summary(np.vstack(y_hat), np.vstack(y_lab), log_dir=save_path + "/" + "test.txt")
-
-        # 각 subject별로 테스트할뿐 concatenate하지 않음
-        else:
-            print("<<<<<<<<<<<< model was trained using all test/train tasks >>>>>>>>>>>>>>>>>")
-            all_au = ['au1', 'au2', 'au4', 'au5', 'au6', 'au9', 'au12', 'au15', 'au17', 'au20', 'au25', 'au26']
-            #all_au=['au12']
-            if FLAGS.test_test:
-                trained_model_dir = FLAGS.keep_train_dir + '/' + 'sbjt' + str(FLAGS.sbjt_start_idx) + ':' + str(
-                    FLAGS.meta_batch_size) + '.ubs_' + str(
-                    FLAGS.train_update_batch_size) + '.numstep' + str(FLAGS.num_updates) + '.updatelr' + str(
-                    FLAGS.train_update_lr) + '.metalr' + str(FLAGS.meta_lr)
-            w_arr = None
-            b_arr = None
-            for au in all_au:
-                model_file = None
-                model_file = tf.train.latest_checkpoint(FLAGS.logdir + '/' + au + '/' + trained_model_dir)
-                print(">>>> model_file from ", au, ": ", model_file)
-                if (model_file == None):
-                    print(
-                        "############################################################################################")
-                    print("####################################################################### None for ", au)
-                    print(
-                        "############################################################################################")
-                else:
-                    if FLAGS.test_iter > 0:
-                        files = os.listdir(model_file[:model_file.index('model')])
-                        if 'model' + str(FLAGS.test_iter) + '.index' in files:
-                            model_file = model_file[:model_file.index('model')] + 'model' + str(FLAGS.test_iter)
-                            print(">>>> model_file2: ", model_file)
-                        else:
-                            print(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", files)
-                    print("Restoring model weights from " + model_file)
-                    saver.restore(sess, model_file)
-                    w = sess.run('model/w1:0')
-                    b = sess.run('model/b1:0')
-                    if w_arr is None:
-                        w_arr = w
-                        b_arr = b
-                    else:
-                        w_arr = np.hstack((w_arr, w))
-                        b_arr = np.vstack((b_arr, b))
-                    print("updated weights from ckpt: ", w, b)
-                    print('----------------------------------------------------------')
-
-            if FLAGS.init_weight and (model_file == None):
-                w_arr = None
-                b_arr = None
-                print(">>>>>>>>>> test robert's model ")
-            test_all(w_arr, b_arr, trained_model_dir)
 
 
 
@@ -653,7 +338,7 @@ def main():
     ################## Train ##################
 
     # train_train or train_test
-    elif FLAGS.resume:  # 디폴트로 resume은 항상 true. 따라서 train중간부터 항상 시작 가능.
+    if FLAGS.resume:  # 디폴트로 resume은 항상 true. 따라서 train중간부터 항상 시작 가능.
         model_file = None
 
         if FLAGS.train_test:
@@ -662,10 +347,10 @@ def main():
                 FLAGS.train_update_batch_size) + '.numstep' + str(FLAGS.num_updates) + '.updatelr' + str(
                 FLAGS.train_update_lr) + '.metalr' + str(FLAGS.meta_lr)
             model_file = tf.train.latest_checkpoint(FLAGS.logdir + '/' + tmp_trained_model_dir)
+            print(">>>>> trained_model_dir: ", FLAGS.logdir + '/' + tmp_trained_model_dir)
         else:
             model_file = tf.train.latest_checkpoint(FLAGS.logdir + '/' + trained_model_dir)
-
-        print(">>>>> trained_model_dir: ", FLAGS.logdir + '/' + trained_model_dir)
+            print(">>>>> trained_model_dir: ", FLAGS.logdir + '/' + trained_model_dir)
 
         w = None
         b = None
