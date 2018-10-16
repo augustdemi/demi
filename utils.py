2b3974225ed78768ae8aa69b06bd6a1f2b9dfa96
augustdemi <augustdemi@gmail.com>
2018-10-16 17:21:58 -0400

    get_kshot_feature 함수 추가

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ def get_images(path, label_int, seed, nb_samples=None, validate=True):
     return off_images, on_images
 
 
-def get_kshot_from_path(path, seed, nb_samples=None, validate=True):
+def get_kshot_from_img_path(path, seed, nb_samples=None, validate=True):
     subject = int(path[-1])
     print("============================================")
     print(">>>>>>>>>>>>>subject: ", subject)
@@ -104,8 +104,65 @@ def get_kshot_from_path(path, seed, nb_samples=None, validate=True):
     return off_images, on_images
 
 
+def get_kshot_feature(kshot_path, feat_path, seed, nb_samples=None, validate=True):
+    subject = int(kshot_path[-1])
+    print("============================================")
+    print(">>>>>>>>>>>>>subject: ", subject)
+    # random seed는 subject에 따라서만 다르도록. 즉, 한 subject내에서는 k가 증가해도 계속 동일한 seed인것.
+    labels = ['off', 'on']  # off = 0, on =1
 
+    # check count of existing samples
+    num_existing_samples = []
+    for label in labels:
+        # on/off 이미지를 구분해 놓은 csv파일로부터 라벨별 이미지 경로 읽어와( au, subject별 on 혹은 off 이미지)
+        img_path_list = open(kshot_path + label + '/file_path.csv').readline().split(',')
+        # 그 경로에 해당하는 feature를 선택
+        # feature 파일이 존재하는 경로
+        feature_file_path = feat_path + subject + '.csv'
+        f = open(feature_file_path, 'r')
+        lines = f.readlines()
+        feat_data = {}
+        for line in lines:
+            line = line.split(',')
+            feat_data.update({line[1], line[2:]})  # key = frame, value = feature vector
+        feature_list = []
+        for i in img_path_list:
+            frame = img_path_list[i].split('/')[-1].split('.')[0]
+            feature_list.append(feat_data[frame])
+        num_existing_samples.append(len(feature_list))
 
+    # make the balance
+    num_samples_to_select = [nb_samples, nb_samples]
+    if num_existing_samples[0] < nb_samples:
+        n_off_samples = 2 * math.floor(num_existing_samples[0] / 2)
+        n_on_samples = 2 * nb_samples - n_off_samples
+        num_samples_to_select = [n_off_samples, n_on_samples]
+    elif num_existing_samples[1] < nb_samples:
+        n_on_samples = 2 * math.floor(num_existing_samples[1] / 2)
+        n_off_samples = 2 * nb_samples - n_on_samples
+        num_samples_to_select = [n_off_samples, n_on_samples]
+    print('num_samples_to_select: ', num_samples_to_select)
+
+    def sampler(path, label, n_samples):
+        print("-------------------------------")
+        print("validate: ", validate)
+        print("label: ", label)
+
+        img_path_list = os.listdir(os.path.join(path, label))
+        if validate:
+            random.seed(subject + seed + 10)
+        else:
+            random.seed(subject + seed)
+        random_imgs = random.sample(img_path_list, n_samples)
+        random_img_path = [os.path.join(path, label, img) for img in random_imgs]
+        return random_img_path
+
+    # 각 task별로 k*2개 씩의 label 과 img담게됨. path = till subject.
+    off_images = sampler(path, labels[0], num_samples_to_select[0])
+    print('num of off_images: ', len(off_images))
+    on_images = sampler(path, labels[1], num_samples_to_select[1])
+    print('num of on_images: ', len(on_images))
+    return off_images, on_images
 
 
 
