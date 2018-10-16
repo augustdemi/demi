1e6108de27745895ac3ce219054d78edd8926157
augustdemi <augustdemi@gmail.com>
2018-10-16 17:21:14 -0400

    compute latent val 함수 제거

--- feature_layers.py
+++ feature_layers.py
@@ -7,7 +7,7 @@ import os
 
 
 class feature_layer:
-    def __init__(self, batch_size, num_au):
+    def __init__(self, batch_size, num_au):  # num_au : will be used for building soft max layer
 
         latent_dim1 = 2048
         latent_dim2 = 500
@@ -31,7 +31,7 @@ class feature_layer:
         self.batch_size = batch_size
         self.latent_dim3 = latent_dim3
 
-    # kdt
+    # 이미 만들어진 vae로 부터 3개 레이어에 대한 weight만 취해옴
     def loadWeight(self, vae_model_name, w=None, b=None, au_index=-1):
 
         trained_model = VAE((160, 240, 1), self.batch_size, self.TOTAL_AU).model_train
@@ -67,69 +67,8 @@ class feature_layer:
             b = b.reshape(1, 2)
             self.model_intensity.layers[-1].set_weights([w, b])
 
-    def computeLatentVal(self, x, vae_model, au_idx):
-        if vae_model.endswith('h5'):
-            self.model_train.load_weights(vae_model)
-        else:
-            print('base vae in interative case: ', vae_model + '/' + os.listdir(vae_model)[0])
-            self.model_train.load_weights(vae_model + '/' + os.listdir(vae_model)[0])
-        ##############################
-        z, _ = self.model_z_intensity.predict(x, batch_size=len(x))
-        print(">>>>>>>>> z shape:", z.shape)
-        layer_dict_whole_vae = dict([(layer.name, layer) for layer in self.model_train.layers])
-        loaded_weight = layer_dict_whole_vae['softmaxpdf_1'].get_weights()
-        ##############################
-        print('[vae_model]shape of loaded_weight in computeLatentVal(): ', loaded_weight[0].shape,
-              loaded_weight[1].shape)
-        if (loaded_weight[1].shape[0] > 1) and (au_idx < self.TOTAL_AU):
-            w = loaded_weight[0][:, au_idx]
-            b = loaded_weight[1][au_idx]
-            loaded_weight = [w.reshape(2000, 1, 2), b.reshape(1, 2)]
-            print('[vae_model]after: shape of loaded_weight in computeLatentVal(): ', loaded_weight[0].shape,
-                  loaded_weight[1].shape)
-        return loaded_weight, z
-
-    # only for test_test.(test_test는 사실 test_train 케이스도 포함임. 그래서 test_train인 경우 = w,b모두 None인 경우, 그냥 로버트 모델을 로드해서 씀)
-    def loadWeight_prev(self, vae_model, w=None, b=None, iterative_au=False):
-        if iterative_au:
-            print("######## dir for iterative load of model: ", vae_model)
-            temp_vae_model = VAE((160, 240, 1), 32, 1)
-            w_arr = None
-            b_arr = None
-            for i in range(self.TOTAL_AU):
-                temp_vae_model.model_train.load_weights(vae_model + '/au' + str(i) + '.h5')
-                for j in range(len(self.model_train.layers) - 1):
-                    loaded = temp_vae_model.model_train.layers[j].get_weights()
-                    self.model_train.layers[j].set_weights(loaded)
-                w = temp_vae_model.model_train.get_weights()[58]
-                b = temp_vae_model.model_train.get_weights()[59]
-                w = w.reshape(2000, 1, 2)
-                b = b.reshape(1, 2)
-                if w_arr is None:
-                    w_arr = w
-                    b_arr = b
-                else:
-                    w_arr = np.hstack((w_arr, w))
-                    b_arr = np.vstack((b_arr, b))
-            print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-            print('w shape: ', w_arr.shape)
-            print('b shape: ', b_arr.shape)
-
-            self.model_train.layers[-1].set_weights([w_arr, b_arr])
-            print("[vae_model]loaded weight from robert : ", self.model_train.get_weights()[58],
-                  self.model_train.get_weights()[59])
-        else:
-            self.model_train.load_weights(vae_model)
-            print("[vae_model]loaded weight from robert : ", self.model_train.get_weights()[58],
-                  self.model_train.get_weights()[59])
-            print("And shape of w: ", self.model_train.get_weights()[58].shape)
-            if w is not None and b is not None:
-                self.model_train.layers[-1].weights[0].load(w)
-                self.model_train.layers[-1].weights[1].load(b)
-                print("[vae_model]loaded weight from maml : ", self.model_train.get_weights()[58],
-                      self.model_train.get_weights()[59])
 
     # only for test_test. 로드한 weight으로 pred값 도출. 배치로 한방에 predict하기 위해 로버트 모델을 쓴것.
     def testWithSavedModel(self, x):
-        _, pred = self.model_z_intensity.predict(x, batch_size=len(x))
+        pred = self.model_intensity.predict(x, batch_size=len(x))
         return pred
