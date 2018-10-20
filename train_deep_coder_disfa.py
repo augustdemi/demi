dac18b00e2da365bab44f37c99e67da3cfa2c2e5
augustdemi <augustdemi@gmail.com>
2018-10-20 15:13:13 -0400

    2048 bug

--- train_deep_coder_disfa.py
+++ train_deep_coder_disfa.py
@@ -129,7 +129,7 @@ emb = Dropout(0.5)(emb)
 latent_feat = Dense(latent_dim1, activation='relu', name='latent_feat')(emb)  # into 2048
 # intermediate = Dense(latent_dim2, activation='relu', name='intermediate')(latent_feat)  # into 500
 z_mean = Dense(latent_dim3, name='z_mean')(latent_feat)  # into latent_dim = 300은. output space의 dim이 될것.
-z_log_sigma = Dense(latent_dim3)(emb)
+z_log_sigma = Dense(latent_dim3)(latent_feat)
 print('==============================')
 print('emb', emb.shape)
 # print('latent_feat', latent_feat.shape)
