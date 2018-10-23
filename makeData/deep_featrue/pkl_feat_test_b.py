dea518b90bbd95b9e9c3c5bbb2bfb171ca3e73af
augustdemi <augustdemi@gmail.com>
2018-10-23 08:15:52 -0400

    bug

--- makeData/deep_featrue/pkl_feat_test_b.py
+++ makeData/deep_featrue/pkl_feat_test_b.py
@@ -12,9 +12,9 @@ subjects.sort()
 all_au = ['au1', 'au2', 'au4', 'au6', 'au9', 'au12', 'au25', 'au26']
 
 for subject in subjects:
-    subject_folder = os.path.join(path, subject)
+    subject_folder = os.path.join(kshot_path, subject)
     files = os.listdir(subject_folder)
-    test_file_paths = open(os.path.join(path, subject, 'file_path.csv'))
+    test_file_paths = open(os.path.join(kshot_path, subject, 'file_path.csv'))
     print('original files len in ', subject, ' : ', len(files))
     frame_idx = [int(path.split('/')[-1].split('.')[0].split('_')[0]) for path in test_file_paths]
 
