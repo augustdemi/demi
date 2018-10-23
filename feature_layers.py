befa1e31d31c096285ae6c07e66579a603ab5ad2
augustdemi <augustdemi@gmail.com>
2018-10-23 19:37:00 -0400

    하나의au로만 만들어진 vae는 three layer가 default load weight 함수 이용하도록 변경

--- feature_layers.py
+++ feature_layers.py
@@ -28,41 +28,50 @@ class feature_layer:
         self.latent_dim3 = latent_dim3
 
     # 이미 만들어진 vae로 부터 3개 레이어에 대한 weight만 취해옴
-    def loadWeight(self, vae_model_name, w=None, b=None, au_index=-1):
+    def loadWeight(self, vae_model_name, au_index, w=None, b=None):
+        if au_index > 7:
+            trained_model = VAE((160, 240, 1), self.batch_size, self.TOTAL_AU).model_train
+            print(">>>>>>>>> model loaded from ", vae_model_name)
+            trained_model.load_weights(vae_model_name + '.h5')
+            #### get weight
+            layer_dict_whole_vae = dict([(layer.name, layer) for layer in trained_model.layers])
+            w_intermediate = layer_dict_whole_vae['intermediate'].get_weights()
+            w_z_mean = layer_dict_whole_vae['z_mean'].get_weights()
+            print('check the last layer of model_train: ', trained_model.layers[-1].name)
+            w_softmaxpdf_1 = trained_model.layers[-1].get_weights()
+            print("[vae_model]loaded weight from VAE : ", w_softmaxpdf_1[1])
 
-        trained_model = VAE((160, 240, 1), self.batch_size, self.TOTAL_AU).model_train
-        print(">>>>>>>>> model loaded from ", vae_model_name)
-        trained_model.load_weights(vae_model_name + '.h5')
-        #### get weight
-        layer_dict_whole_vae = dict([(layer.name, layer) for layer in trained_model.layers])
-        w_intermediate = layer_dict_whole_vae['intermediate'].get_weights()
-        w_z_mean = layer_dict_whole_vae['z_mean'].get_weights()
-        print('check the last layer of model_train: ', trained_model.layers[-1].name)
-        w_softmaxpdf_1 = trained_model.layers[-1].get_weights()
-        print("[vae_model]loaded weight from VAE : ", w_softmaxpdf_1[1])
+            # whene w and b is not None = w and b is from MAML
+            if w is not None and b is not None:
+                w_softmaxpdf_1 = [w, b]
+                print("[vae_model]loaded weight from MAML : ", w_softmaxpdf_1[1])
 
-        # whene w and b is not None = w and b is from MAML
-        if w is not None and b is not None:
-            w_softmaxpdf_1 = [w, b]
-            print("[vae_model]loaded weight from MAML : ", w_softmaxpdf_1[1])
+            #### set weight for 3 layers
+            layer_dict_3layers = dict([(layer.name, layer) for layer in self.model_intensity.layers])
+            layer_dict_3layers['intermediate'].set_weights(w_intermediate)
+            layer_dict_3layers['z_mean'].set_weights(w_z_mean)
+            print('check the last layer of model_intensity: ', self.model_intensity.layers[-1].name)
+            trained_model.summary()
+            self.model_intensity.summary()
 
-        #### set weight for 3 layers
-        layer_dict_3layers = dict([(layer.name, layer) for layer in self.model_intensity.layers])
-        layer_dict_3layers['intermediate'].set_weights(w_intermediate)
-        layer_dict_3layers['z_mean'].set_weights(w_z_mean)
-        print('check the last layer of model_intensity: ', self.model_intensity.layers[-1].name)
-        trained_model.summary()
-        self.model_intensity.summary()
-
-        if w_softmaxpdf_1[1].shape[0] == self.num_au:
-            self.model_intensity.layers[-1].set_weights(w_softmaxpdf_1)
+            if w_softmaxpdf_1[1].shape[0] == self.num_au:
+                self.model_intensity.layers[-1].set_weights(w_softmaxpdf_1)
+            else:
+                print(">>>>>>>>>>> going to choose this index in VAE:", au_index)
+                w = w_softmaxpdf_1[0][:, au_index]
+                b = w_softmaxpdf_1[1][au_index]
+                w = w.reshape(self.latent_dim3, 1, 2)
+                b = b.reshape(1, 2)
+                self.model_intensity.layers[-1].set_weights([w, b])
         else:
-            print(">>>>>>>>>>> going to choose this index in VAE:", au_index)
-            w = w_softmaxpdf_1[0][:, au_index]
-            b = w_softmaxpdf_1[1][au_index]
-            w = w.reshape(self.latent_dim3, 1, 2)
-            b = b.reshape(1, 2)
-            self.model_intensity.layers[-1].set_weights([w, b])
+            self.model_intensity.load_weight(vae_model_name + '.h5')
+            # whene w and b is not None = w and b is from MAML
+            if w is not None and b is not None:
+                w_softmaxpdf_1 = [w, b]
+                print("[vae_model]loaded weight from MAML : ", w_softmaxpdf_1[1])
+            self.model_intensity.layers[-1].set_weights(w_softmaxpdf_1)
+
+
 
     def loadWeight_m0(self, vae_model_name, w, b, au_index=-1):
 
