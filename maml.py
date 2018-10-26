d94a2755a403dec401afecdb7bd728abbd76ad90
augustdemi <augustdemi@gmail.com>
2018-10-26 17:00:25 -0400

    bug

--- maml.py
+++ maml.py
@@ -91,7 +91,7 @@ class MAML:
                 task_outputbs.append(output)
                 task_labelbs.append(labelb)
                 task_lossesb.append(self.loss_func(output, labelb))
-                all_fast_weights = [fast_weights['w1'], fast_weights['b1']]
+                all_fast_weights = [[fast_weights['w1'], fast_weights['b1']]]
 
                 for j in range(num_updates - 1):
                     loss = self.loss_func(self.forward(inputa, fast_weights, reuse=True), labela)
