f1e4f28908d161538b7f822127051707145894f2
augustdemi <augustdemi@gmail.com>
2018-10-26 00:15:11 -0400

    피처 형성을 위한 모델 로드를 무조건 total au 갯수로 하도록

--- data_generator.py
+++ data_generator.py
@@ -122,13 +122,8 @@ class DataGenerator(object):
 
         batch_size = 10
         three_layers = feature_layer(batch_size, FLAGS.num_au)
-        if not FLAGS.train:
-            if FLAGS.model.startswith('s4'):
-                three_layers.loadWeight(os.path.join(FLAGS.vae_model, os.listdir(FLAGS.vae_model)[0].split('.')[0]),
-                                        FLAGS.au_idx,
-                                        num_au_for_rm=FLAGS.num_au)
-        else:
-            three_layers.loadWeight(FLAGS.vae_model, FLAGS.au_idx, num_au_for_rm=FLAGS.num_au)
+        three_layers.loadWeight(FLAGS.vae_model, FLAGS.au_idx, num_au_for_rm=FLAGS.num_au)
+
         inputa_latent_feat = three_layers.model_final_latent_feat.predict(inputa_features)
         inputb_latent_feat = three_layers.model_final_latent_feat.predict(inputb_features)
         print(">>> z_arr len:", len(inputa_latent_feat))
