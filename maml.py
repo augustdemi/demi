a06c77343b1bd1f3ed909cfe54f557cca03f4116
augustdemi <augustdemi@gmail.com>
2018-10-27 02:19:10 -0400

    all_fast_weight 없앰

--- maml.py
+++ maml.py
@@ -92,8 +92,6 @@ class MAML:
                 task_outputbs.append(output)
                 task_labelbs.append(labelb)
                 task_lossesb.append(self.loss_func(output, labelb))
-                all_w.append(fast_weights['w1'])
-                all_b.append(fast_weights['b1'])
 
                 for j in range(num_updates - 1):
                     loss = self.loss_func(self.forward(inputa, fast_weights, reuse=True), labela)
@@ -110,8 +108,7 @@ class MAML:
                     task_outputbs.append(output)
                     task_labelbs.append(labelb)
                     task_lossesb.append(self.loss_func(output, labelb))
-                    all_w.append(fast_weights['w1'])
-                    all_b.append(fast_weights['b1'])
+
 
                 task_accuracya = tf.contrib.metrics.accuracy(tf.argmax(tf.nn.softmax(task_outputa), 1),
                                                              tf.argmax(labela, 1))
@@ -120,19 +117,16 @@ class MAML:
                                                                         tf.argmax(labelb, 1)))
 
                 task_output = [task_outputa, task_outputbs, labela, task_labelbs, task_lossa, task_lossesb,
-                               task_accuracya, task_accuraciesb, all_w, all_b,
-                               [fast_weights['w1'], fast_weights['b1']]]
+                               task_accuracya, task_accuraciesb, [fast_weights['w1'], fast_weights['b1']]]
                 return task_output
 
             out_dtype = [tf.float32, [tf.float32] * num_updates, tf.float32, [tf.float32] * num_updates, tf.float32,
-                         [tf.float32] * num_updates, tf.float32, [tf.float32] * num_updates,
-                         [tf.float32] * num_updates, [tf.float32] * num_updates,
-                         [tf.float32, tf.float32]]
+                         [tf.float32] * num_updates, tf.float32, [tf.float32] * num_updates, [tf.float32, tf.float32]]
 
             result = tf.map_fn(task_metalearn, elems=(self.inputa, self.inputb, self.labela, self.labelb),
                                dtype=out_dtype, parallel_iterations=FLAGS.meta_batch_size)
             # In result, outa has shape (1,2,1,2) = (num.of.task, 2*k, num.of.au, one-hot label)
-            outputas, outputbs, res_labela, res_labelbs, lossesa, lossesb, accuraciesa, accuraciesb, all_w, all_b, fast_weights = result
+            outputas, outputbs, res_labela, res_labelbs, lossesa, lossesb, accuraciesa, accuraciesb, fast_weights = result
 
         ## Performance & Optimization
         if 'train' in prefix:
@@ -141,7 +135,7 @@ class MAML:
             self.total_losses2 = total_losses2 = [tf.reduce_sum(lossesb[j]) / tf.to_float(FLAGS.meta_batch_size) for j
                                                   in range(num_updates)]
             # after the map_fn
-            self.outputas, self.outputbs, self.all_w, self.all_b, self.fast_weights = outputas, outputbs, all_w, all_b, fast_weights
+            self.outputas, self.outputbs, self.fast_weights = outputas, outputbs, fast_weights
             if self.classification:
                 self.total_accuracy1 = total_accuracy1 = tf.reduce_sum(accuraciesa) / tf.to_float(FLAGS.meta_batch_size)
                 self.total_accuracies2 = total_accuracies2 = [
