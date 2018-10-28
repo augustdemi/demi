9c1773c2cc7715da17f70e4a5bfef8063979b77d
augustdemi <augustdemi@gmail.com>
2018-10-28 04:39:18 -0400

    num_updates만큼 텐서 생성하도록 수정

--- main.py
+++ main.py
@@ -211,41 +211,46 @@ def train(model, saver, sess, trained_model_dir, metatrain_input_tensors, metava
 def inner_update(model, saver, sess, trained_model_dir, metatrain_input_tensors, resume_itr):
     print("===============> Final in weight: ", sess.run('model/w1:0').shape, sess.run('model/b1:0').shape)
 
+
     feed_dict = {model.inputa: metatrain_input_tensors['inputa'].eval(),
                  model.inputb: metatrain_input_tensors['inputb'].eval(),
                  model.labela: metatrain_input_tensors['labela'].eval(),
-                 model.labelb: metatrain_input_tensors['labelb'].eval(), model.meta_lr: FLAGS.meta_lr}
+                 model.labelb: metatrain_input_tensors['labelb'].eval(), model.meta_lr: 0}
     print('Done initializing, starting training.')
+    input_tensors = [model.metatrain_op, model.all_w, model.all_b, model.total_losses2]
 
-    losses = []
-    prev_weight = []
-    final_iteration = FLAGS.metatrain_iterations
-    for itr in range(resume_itr + 1, FLAGS.metatrain_iterations + 1):
+    result = sess.run(input_tensors, feed_dict)
 
-        input_tensors = [model.pretrain_op, model.total_loss1]
+    # save local weight as a global weight
 
-        result = sess.run(input_tensors, feed_dict)
-        # save local weight as a global weight
+    loss = np.array(result[3])
+    # print('loss per update: ', loss)
+    print('>>> num of update: ', len(loss))
+    # early_stop_iter = FLAGS.num_updates - 1
+    # for i in range(1, FLAGS.num_updates):
+    #     if loss[i] > loss[i - 1]:
+    #         print("loss inc at iteration: ", i, loss[i - 1], loss[i])
+    #         early_stop_iter = i - 1
+    #         break
+    all_w = result[1]
+    all_b = result[2]
+    print('>>> shape of local_weights:', np.array(all_w).shape)
+    # print('!!!!!!!!!! early stop at : ', early_stop_iter)
 
-        loss = result[1]
-        if itr > 1:
-            reduced_loss = losses[-1] - loss
-            print("reduced loss : ", reduced_loss)
-            # if reduced_loss <= 0:
-            #     print('!!!!!!!!!! early stop at : ', itr)
-            #     final_iteration = itr
-            #     break
-        losses.append(loss)
-        print('>>>>>> Current Global weights: ', sess.run('model/b1:0'))
-        prev_weight = [sess.run('model/w1:0'), sess.run('model/b1:0')]
-
-    model.weights['w1'].load(prev_weight[0], sess)
-    model.weights['b1'].load(prev_weight[1], sess)
-    print('>>>>>> saved Global weights: ', sess.run('model/b1:0'))
+    # for i in range(len(all_b)):
+    #     print(i, all_b[i][0])  # index: update_batch_size, meta_batch_size
+    local_w = all_w[FLAGS.num_updates - 1][0]
+    local_b = all_b[FLAGS.num_updates - 1][0]
+    print("================================================================================")
+    print('>>>>>> Current Global weights: ', sess.run('model/b1:0'))
+    model.weights['w1'].load(local_w, sess)
+    model.weights['b1'].load(local_b, sess)
+    print('>>>>>> Updated Local weights ', sess.run('model/b1:0'))
     save_path = FLAGS.logdir + '/' + trained_model_dir
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    saver.save(sess, save_path + '/model' + str(final_iteration))
+    saver.save(sess, save_path + '/model' + str(FLAGS.num_updates))
+
 
 
 def main():
