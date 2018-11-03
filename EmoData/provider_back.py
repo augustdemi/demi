7959ed050ac125b86560f0fc475cb15d45a18d09
augustdemi <augustdemi@gmail.com>
2018-11-03 17:19:06 -0400

    float to int

--- EmoData/provider_back.py
+++ EmoData/provider_back.py
@@ -357,12 +357,13 @@ def flow_from_kshot_feat(path_to_folder, feature_path, kshot_seed,
     inputb_features = []
     labelas = []
     labelbs = []
+    print(">>>>>> per label samples :", update_batch_size * 2)
     print('subject_folders: ', subject_folders)
     # To have totally different inputa and inputb, they should be sampled at the same time and then splitted.
     for sub_folder in folders:  # 쓰일 task수만큼만 경로 만든다. 이 task들이 iteration동안 어차피 반복될거니까
         # random.shuffle(sampled_character_folders)
         off_feat, on_feat = get_kshot_feature(sub_folder, feature_path, kshot_seed,
-                                              nb_samples=update_batch_size * 2, validate=False)
+                                              nb_samples=int(update_batch_size * 2), validate=False)
         # Split data into a/b
         half_off_img = int(len(off_feat) / 2)
         half_on_img = int(len(on_feat) / 2)
