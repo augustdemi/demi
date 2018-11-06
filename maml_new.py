04846c989437e4dd77504b797f63d6629aabc171
augustdemi <augustdemi@gmail.com>
2018-11-06 02:42:01 -0500

    regularize1 with 1/2

--- maml_new.py
+++ maml_new.py
@@ -27,7 +27,7 @@ class MAML:
         self.weight_dim = 300
         self.total_num_au = 8
         self.num_classes = 2
-        self.LAMBDA1 = 0
+        self.LAMBDA1 = 1 / 2
         self.au_idx = -1
         if FLAGS.datasource == 'disfa':
             self.loss_func = xent
