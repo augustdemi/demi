c22372fa9cb28a89f57142f2e20fcfb9dc7cc4c7
augustdemi <augustdemi@gmail.com>
2019-01-28 11:35:02 +0000

    loss dim 조정

--- maml_new.py
+++ maml_new.py
@@ -105,18 +105,18 @@ class MAML:
                     # pred_other_au = tf.nn.softmax(pred_other_au)
                     # pred_other_au = tf.cast(tf.argmax(pred_other_au[:, 0], 1), tf.float32)
                     label_other_au = tf.cast(label[:, i], tf.float32)
-                    pred_other_au = label_other_au
+                    pred_other_au = tf.cast(label[:, i], tf.float32)
                     return [pred_other_au, label_other_au]
 
                 task_co_lossa = []
                 for i in range(self.total_num_au):
                     pred_other_au, label_other_au = predict_other_au(i, inputa, labela)
-                    # sample 갯수만큼이 reduced sum된 per au and per subject의 loss가 생김
+
                     loss = self.loss_func2(pred_this_au * pred_other_au,
                                            labela_this_au * label_other_au)  # (num of samples=NK,1=num of au,2=N)
                     task_co_lossa.append(
                         loss)  # losses 는 현재 주어진 subject이, between 현재 주어진 au and 다른 모든 au간 이룬 loss들의 모임.
-                task_co_lossa = tf.reduce_sum(task_co_lossa)
+                task_co_lossa = tf.reduce_sum(task_co_lossa, 1) / self.total_num_au
                 task_lossa = task_ce_lossa + self.LAMBDA2 * task_co_lossa
 
                 grads = tf.gradients(task_lossa, list(this_weight.values()))  # 2000,1,2
@@ -137,7 +137,7 @@ class MAML:
                         # (num of samples=NK,1=num of au,2=N)
                         task_co_loss.append(self.loss_func2(pred_this_au * pred_other_au,
                                                             labela_this_au * label_other_au))
-                    task_co_loss = tf.reduce_sum(task_co_loss)
+                    task_co_loss = tf.reduce_sum(task_co_loss, 1) / self.total_num_au
                     task_loss = task_ce_loss + self.LAMBDA2 * task_co_loss
                     # compute gradients based on the previous fast weights
                     grads = tf.gradients(task_loss, list(fast_weights.values()))
@@ -161,8 +161,8 @@ class MAML:
                     predb_other_au, labelb_other_au = predict_other_au(i, inputb, labelb)
                     task_co_lossb.append(
                         self.loss_func2(predb_this_au * predb_other_au, labelb_this_au * labelb_other_au))
-                task_co_lossb = tf.reduce_mean(task_co_lossb)
-                task_total = tf.reduce_sum(task_ce_lossb) + self.LAMBDA2 * task_co_lossb
+                task_co_lossb = tf.reduce_sum(task_co_lossb, 1) / self.total_num_au
+                task_total = task_ce_lossb + self.LAMBDA2 * task_co_lossb
                 ### return output ###
                 task_output = [fast_weights['w1'], fast_weights['b1'], task_ce_lossb, task_co_lossb, task_total]
                 return task_output
@@ -192,6 +192,14 @@ class MAML:
         # 8*14 --> 8*1 (make each 1*14 into 1*1)
         self.total_losses = [tf.reduce_sum(self.task_total_losses[k]) / tf.to_float(FLAGS.meta_batch_size) for k in
                              range(self.total_num_au)]
+
+        print('-------------------------------- shape test ----------------------------------')
+        print(self.task_ce_losses[0].shape)
+        print(self.task_co_losses[0].shape)
+        print(self.task_total_losses[0].shape)
+        print(self.total_losses[0].shape)
+        print('----------------------------------------')
+
 
         ## Performance & Optimization
         tf.summary.scalar('CE_AU1', tf.reduce_sum(self.task_ce_losses[0]) / tf.to_float(FLAGS.meta_batch_size))
