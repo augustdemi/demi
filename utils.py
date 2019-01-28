5c76dd36bc08f1c83ae14eb4b807992cd3a3541e
augustdemi <augustdemi@gmail.com>
2019-01-28 11:53:33 +0000

    mse에서 reduce mean 제거

--- utils.py
+++ utils.py
@@ -293,7 +293,7 @@ def normalize(inp, activation, reuse, scope):
 def mse(pred, label):
     pred = tf.reshape(pred, [-1])
     label = tf.reshape(label, [-1])
-    return tf.reduce_mean(tf.square(pred-label))
+    return tf.square(pred - label) / FLAGS.update_batch_size
 
 def xent(pred, label):
     # Note - with tf version <=0.12, this loss has incorrect 2nd derivatives
