16e39be2a3d26fa2a5bca7bcc5498a2be2200ac4
augustdemi <augustdemi@gmail.com>
2019-02-07 17:15:20 +0000

    concatenate

--- test2.py
+++ test2.py
@@ -305,15 +305,15 @@ def main():
             result = test_each_subject(w_arr, b_arr, subj_idx)
             y_hat.append(result[0])
             y_lab.append(result[1])
-            print("y_hat shape:", result[0].shape)
-            print("y_lab shape:", result[1].shape)
-            out = print_summary(result[0], result[1], log_dir=save_path + "/test.txt")
-            print('here!!!!!!!!')
-            print(out['data'])
-            #     print(">> y_hat_all shape:", np.vstack(y_hat).shape)
-            #     print(">> y_lab_all shape:", np.vstack(y_lab).shape)
-            # print_summary(np.vstack(y_hat), np.vstack(y_lab),
-            #               log_dir=save_path + "/test.txt")
+            # print("y_hat shape:", result[0].shape)
+            # print("y_lab shape:", result[1].shape)
+            # out = print_summary(result[0], result[1], log_dir=save_path + "/test.txt")
+            # print('here!!!!!!!!')
+            # print(out['data'])
+            print(">> y_hat_all shape:", np.vstack(y_hat).shape)
+            print(">> y_lab_all shape:", np.vstack(y_lab).shape)
+            print_summary(np.vstack(y_hat), np.vstack(y_lab),
+                          log_dir=save_path + "/test.txt")
 
     end_time = datetime.now()
     elapse = end_time - start_time
