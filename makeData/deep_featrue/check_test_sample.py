f9bb11109cfc784130cd017a5064830705cefb12
augustdemi <augustdemi@gmail.com>
2019-02-13 14:27:10 +0000

    update

--- makeData/deep_featrue/check_test_sample.py
+++ makeData/deep_featrue/check_test_sample.py
@@ -1,7 +1,7 @@
 import pickle
 import os
 
-path = '/home/ml1323/project/robert_code/new/check_labels/'
+path = '/home/ml1323/project/robert_code/new/check_labels/test/5shot/'
 files = os.listdir(path)
 
 test_subjects = ['SN017', 'SN018', 'SN021', 'SN023', 'SN024', 'SN025', 'SN026', 'SN027', 'SN028', 'SN029', 'SN030',
@@ -9,9 +9,9 @@ test_subjects = ['SN017', 'SN018', 'SN021', 'SN023', 'SN024', 'SN025', 'SN026', 
 
 num_frame = {}
 
-for subject_idx in range(13, 13):
-    print('=================== subject_idx: ', subject_idx)
-    one_subj_files = [f for f in files if f.startswith(str(subject_idx) + '_')]
+for subject in test_subjects:
+    print('=================== subject_idx: ', subject)
+    one_subj_files = [f for f in files if f.startswith(subject)]
     frames = []
     for file in one_subj_files:
         print('-----------------------------')
@@ -22,5 +22,11 @@ for subject_idx in range(13, 13):
         frames.extend(contents['off'])
         frames.extend(contents['on'])
     frames = set(frames)
-    num_frame.update({subject_idx: len(frames)})
+    num_frame.update({subject: len(frames)})
+    # with open(path + 'statistics.csv', 'w') as f:
+    #     for i in num_frame:
+    #         out_csv = np.hstack(
+    #             (subject, "frame" + str(detected_frame_idx[i]), [str(x) for x in deep_feature[i]]))
+    #         f.write(','.join(out_csv) + '\n')
+    # print(">>>>>>>>done: ", subject, len(deep_feature))
 print(num_frame)
