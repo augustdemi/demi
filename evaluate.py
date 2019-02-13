5b59c71250d3c719f66334a4c2cdd3c445a1b5f0
augustdemi <augustdemi@gmail.com>
2019-02-13 23:50:30 +0000

    bug

--- evaluate.py
+++ evaluate.py
@@ -12,6 +12,8 @@ for subject_idx in range(12):
     file = pickle.load(open(path + '/predicted_subject' + str(subject_idx) + '.pkl', 'rb'), encoding='latin1')
     y_lab = file['y_lab']
     y_hat = file['y_hat']
+    y_lab_all.append(y_lab)
+    y_hat_all.append(y_hat)
     out = print_summary(y_hat, y_lab, log_dir="./logs/result/" + "/test.txt")
     f1_scores.append(out['data'][5])
 
