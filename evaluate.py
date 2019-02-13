cd1621fb7900ad9f235b60cd6c74add8aecc3de7
augustdemi <augustdemi@gmail.com>
2019-02-13 23:42:56 +0000

    bug

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('s', nargs='?', const=0, type=int, help='subject start idx to evaluate')
 parser.add_argument('n', nargs='?', const=12, type=int, help='number of test tasks to evaluate')
-parser.add_argument('path', type=str, help='where predicted file saved')
+parser.add_argument('p', type=str, help='where predicted file saved')
 
 args = parser.parse_args()
 print(args.accumulate(args.integers))
@@ -15,7 +15,7 @@ y_lab_all = []
 y_hat_all = []
 f1_scores = []
 for subject_idx in range(args.s, args.s + args.n):
-    file = pickle.load(open(args.path + 'predicted_subject' + str(subject_idx) + '.pkl', 'rb'), encoding='latin1')
+    file = pickle.load(open(args.p + 'predicted_subject' + str(subject_idx) + '.pkl', 'rb'), encoding='latin1')
     y_lab = file['y_lab']
     y_hat = file['y_hat']
     out = print_summary(y_hat, y_lab, log_dir="./logs/result/" + "/test.txt")
