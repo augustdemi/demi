8f673a2328080fcd4d2bf896bc9996ac7afba748
augustdemi <augustdemi@gmail.com>
2019-02-13 10:25:49 +0000

    change name

--- makeData/deep_featrue/check_sample.py
+++ makeData/deep_featrue/check_sample.py
@@ -9,13 +9,18 @@ test_subjects = ['SN017', 'SN018', 'SN021', 'SN023', 'SN024', 'SN025', 'SN026', 
 
 num_frame = {}
 
-for subject in test_subjects:
-    one_subj_files = [f for f in files if f.startswith(subject)]
+for subject_idx in range(13):
+    print('=================== subject_idx: ', subject_idx)
+    one_subj_files = [f for f in files if f.startswith(subject_idx)]
     frames = []
     for file in one_subj_files:
+        print('-----------------------------')
+        print('file name: ', file)
         contents = pickle.load(open(path + file, 'rb'), encoding='latin1')
+        print('off: ', contents['off'])
+        print('on: ', contents['on'])
         frames.extend(contents['off'])
         frames.extend(contents['on'])
     frames = set(frames)
-    num_frame.update({subject: len(frames)})
+    num_frame.update({subject_idx: len(frames)})
 print(num_frame)
