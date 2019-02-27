615f78be2fa2a71c2ffed30d21c58ee3e0a1b6c2
augustdemi <augustdemi@gmail.com>
2019-02-27 17:59:29 +0000

    bug

--- EmoData/provider_back.py
+++ EmoData/provider_back.py
@@ -476,7 +476,7 @@ def flow_from_kshot_csv(used_info_path, feature_path, label_path, subject_index,
         with open(os.path.join(label_path, subject, subject + '_' + au + '.txt'), 'r') as f:
             lines = f.readlines()[:4845]
         labels_per_subj_per_au = [binary_intensity(np.float32(line.split(',')[1].split('\n')[0])) for line in
-                                  lines[used_frames]]
+                                  np.array(lines)[used_frames]]
         if len(labels_per_subj_per_au) < 4845:
             labels_per_subj_per_au.append(-1)
             labels_per_subj.append(labels_per_subj_per_au)
