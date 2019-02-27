767042b44502d1c66a37ad3793280cf94f65951f
augustdemi <augustdemi@gmail.com>
2019-02-27 03:02:25 +0000

    only 300 dim

---
+++ one_layer.py
@@ -0,0 +1,54 @@
+import EmoEstimator as EE
+from build_vae import VAE
+from keras.layers import Dense, Input, Reshape
+import keras as K
+
+
+class feature_layer:
+    def __init__(self, batch_size, num_au):  # num_au : will be used for building soft max layer
+
+
+        latent_dim3 = 300
+        num_of_intensity = 2
+        TOTAL_AU = 8
+
+        ################# From here, reconstruct the model from input = 2048 with only 3 required layers to finetune only softmax layer
+        inp_1 = Input(shape=[latent_dim3])
+        out_1 = EE.layers.softmaxPDF(num_au, num_of_intensity)(Reshape((latent_dim3, 1))(inp_1))
+
+        model_intensity = K.models.Model([inp_1], [out_1])
+        # model_final_latent_feat = K.models.Model([inp_1], [z_mean])
+
+        # self.model_final_latent_feat = model_final_latent_feat
+        self.model_intensity = model_intensity
+        self.TOTAL_AU = TOTAL_AU
+        self.num_au = num_au
+        self.batch_size = batch_size
+        self.latent_dim3 = latent_dim3
+
+    # 이미 만들어진 vae로 부터 3개 레이어에 대한 weight만 취해옴
+    def loadWeight(self, vae_model_name, au_index=-1, w=None, b=None):
+        trained_model = VAE((160, 240, 1), self.batch_size, self.num_au).model_train
+        print(">>>>>>>>> model loaded from ", vae_model_name)
+        trained_model.load_weights(vae_model_name + '.h5')
+        print('check the last layer of model_train: ', trained_model.layers[-1].name)
+        w_softmaxpdf_1 = trained_model.layers[-1].get_weights()
+        # whene w and b is not None = w and b is from MAML
+        if w is not None and b is not None:
+            w_softmaxpdf_1 = [w, b]
+            print("[vae_model]loaded weight from MAML : ", w_softmaxpdf_1[1])
+
+        print('check the last layer of model_intensity: ', self.model_intensity.layers[-1].name)
+
+        if au_index < 0:
+            self.model_intensity.layers[-1].set_weights(w_softmaxpdf_1)
+        else:
+            print(">>>>>>>>>>> going to choose this index in VAE:", au_index)
+            try:
+                w = w_softmaxpdf_1[0][:, au_index]
+                b = w_softmaxpdf_1[1][au_index]
+                w = w.reshape(self.latent_dim3, 1, 2)
+                b = b.reshape(1, 2)
+                self.model_intensity.layers[-1].set_weights([w, b])
+            except IndexError as err:
+                print("###########################IndexError:", err)
